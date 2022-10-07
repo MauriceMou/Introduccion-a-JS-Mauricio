415c771b7b3081ebb5f3fa3c7ef999502bbd1977
MauriceMou <mauricio.delgreco22@gmail.com>
2022-10-07 18:18:53 -0300

    arregla creado de inputs
    con estos cambio se logra el arreglo del problema en el cual no se creaba mas de un input para colocar el tiempo

--- Clase-5/tarea-3/tarea-3-mauricio.js
+++ Clase-5/tarea-3/tarea-3-mauricio.js
@@ -5,51 +5,42 @@
 // cada dato.
 // al apretar el botón "Calcular tiempo total", debe mostrar en un
 // <strong> pre-creado el tiempo total de los videos.
+const botonCrearClase = document.querySelector("#agregar");
+const botonReset = document.querySelector("#reset");
+const botonCalcular = document.querySelector("#calcular-total");
 
-
-const botonCrearClase = document.querySelector('#agregar');
-const botonReset = document.querySelector('#reset');
-const botonCalcular = document.querySelector('#calcular-total');
-
-const $nodoMaster = document.querySelector('body');
-const $div = document.createElement('div');
-const $form = document.createElement('form');
-const $label = document.createElement('label');
-const $inputHoras = document.createElement('input');
-const $inputMinutos = document.createElement('input');
-const $inputSegundos = document.createElement('input');
-const $primerParrafo = document.createElement('p');
-const $segundoParrafo = document.createElement('p');
-const $tercerParrafo = document.createElement('p');
-
-
-
+function createElemByTag(tag, isNumber) {
+    const elem = document.createElement(tag);
+    if (isNumber) {
+        elem.setAttribute("type", "number");
+    }
+    return elem;
+}
 
 function crearInput() {
-    //$nodoMaster.append($div, $form, $primerParrafo, $inputHoras, $segundoParrafo, $inputMinutos, $tercerParrafo, $inputSegundos);
-    $nodoMaster.appendChild($div)
-    $div.appendChild($form)
-    $form.appendChild($inputHoras)
-    $form.appendChild($inputMinutos)
-    $form.appendChild($inputSegundos)
-    $form.setAttribute('class', 'formulario');
-    $inputHoras.setAttribute('type', 'Number');
-    $inputMinutos.setAttribute('type', 'Number');
-    $inputSegundos.setAttribute('type', 'Number');
-    $inputHoras.setAttribute('class', 'horas');
-    $inputMinutos.setAttribute('class', 'minutos');
-    $inputSegundos.setAttribute('class', 'segundos');
-    $inputHoras.setAttribute('placeholder', 'HORAS');
-    $inputMinutos.setAttribute('placeholder', 'MINUTOS');
-    $inputSegundos.setAttribute('placeholder', 'SEGUNDOS');
+    const $nodoMaster = document.querySelector("body");
+
+    const div = createElemByTag("div");
+
+    $nodoMaster.appendChild(div);
+
+    const form = createElemByTag("form");
+    div.appendChild(form);
+    const horas = createElemByTag("input", true);
+    horas.setAttribute("placeholder", "HORAS");
+    form.appendChild(horas);
+    const minutos = createElemByTag("input", true);
+    minutos.setAttribute("placeholder", "MINUTOS");
+    form.appendChild(minutos);
+    const segundos = createElemByTag("input", true);
+    segundos.setAttribute("placeholder", "SEGUNDOS");
+    form.appendChild(segundos);
 }
 
 botonCrearClase.onclick = function () {
-    const cantidadDeClases = document.querySelector('#clases').value
-    for(let i= 0; i <= cantidadDeClases; i++){
-        crearInput()
+    const cantidadDeClases = document.querySelector("#clases").value;
+    for (let i = 0; i <= cantidadDeClases; i++) {
+        crearInput();
     }
-
-}
-
+};
 
