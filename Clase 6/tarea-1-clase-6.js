17badefb43d00981b1e073af0cc396f3c3a48cb7
MauriceMou <mauricio.delgreco22@gmail.com>
2022-10-27 00:02:40 -0300

    completo tarea 1
    se le agregan todas las funcionalidades a los botones, se realizan todos los calculos correspondientes para el correcto funcionamiento de la pagina web.
    tambien se arreglan un par de errores en el html y se completa el esqueleto final

--- Clase 6/tarea-1-clase-6.js
+++ Clase 6/tarea-1-clase-6.js
@@ -7,38 +7,67 @@ Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuev
 */
 const botonIntegrantes = document.querySelector('#enviar-integrantes');
 const cantidadDeIntegrantes = document.querySelector('#integrantes');
+const botonCalcular = document.querySelector('#calcular');
+const botonReset = document.querySelector('#reset');
 
-botonIntegrantes.onclick = function () {
+botonIntegrantes.onclick = function() {
     event.preventDefault();
     for (let N = 1; N <= cantidadDeIntegrantes.value; N++) {
-        const $nodoMaster = document.querySelector('body');
-        const div = document.querySelector('div');
         const form = document.querySelector('#formulario-nuevo');
         const br = document.createElement('br');
 
-        $nodoMaster.appendChild(div);
-        div.appendChild(form);
         form.appendChild(crearLabel(N));
         form.appendChild(crearInput(N));
         form.appendChild(br);
+        botonCalcular.disabled = false;
+        botonReset.disabled = false;
+        botonIntegrantes.disabled = true;
+        cantidadDeIntegrantes.disabled = true;
 
     }
 };
 
 function crearLabel(N) {
     let labelIntregrante = document.createElement('label');
-    labelIntregrante.id = 'label-edades-' + N
+    labelIntregrante.id = 'label-edades-' + N;
     labelIntregrante.setAttribute = 'for', 'input-edades-' + N;
     labelIntregrante.textContent = 'Edad de integrante N°' + N;
 
     return labelIntregrante;
 }
+
 function crearInput(N) {
     let inputIntegrante = document.createElement('input');
     inputIntegrante.id = 'input-edades-' + N;
+    inputIntegrante.className = 'edad-usuario';
     inputIntegrante.type = 'number';
 
     return inputIntegrante;
 }
+botonCalcular.onclick = function() {
+    event.preventDefault();
+    const arrayResultados = [];
+    const $nodoResultados = document.querySelectorAll('.edad-usuario');
+    for (let i = 0; i < $nodoResultados.length; i++) {
+        arrayResultados[i] = Number($nodoResultados[i].value);
+    }
+    const $resultadoMayor = document.querySelector('#mayor');
+    const $resultadoMenor = document.querySelector('#menor');
+    const $resultadoPromedio = document.querySelector('#promedio');
 
+    let acumuladorPromedio = 0;
+    for (let i = 0; i < arrayResultados.length; i++) {
+        acumuladorPromedio += arrayResultados[i];
+    }
+    const promedioFinal = acumuladorPromedio / arrayResultados.length;
+    $resultadoPromedio.textContent = `El promedio de edad del grupo familiar es de ${promedioFinal}`;
 
+    const menorFinal = Math.min(...arrayResultados);
+    $resultadoMenor.textContent = `La menor edad del grupo familiar es ${menorFinal}`;
+
+    const mayorFinal = Math.max(...arrayResultados);
+    $resultadoMayor.textContent = `La mayor edad del grupo familiar es ${mayorFinal}`;
+};
+botonReset.onclick = function() {
+    document.querySelector('#formulario-nuevo').innerHTML = '';
+}
